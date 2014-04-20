90e7dc84fad853f58a42ff0b86f3bf6f47bf98be
François Poirotte <clicky@erebot.net>
2014-04-20 14:26:07 +0200

    Add ngircd configuration.

---
+++ ngircd/manifests/init.pp
@@ -0,0 +1,40 @@
+class ngircd {
+	$oper_login = extlookup('irc_oper_login')
+	$oper_pass  = extlookup('irc_oper_pass')
+
+	service {
+	"ngircd":
+		require => File['/etc/ngircd/ngircd.conf'],
+	}
+
+	package {
+	"ngircd":
+	}
+
+	file {
+	"/etc/ngircd/ngircd.conf":
+		source => template('ngircd/ngircd.conf'),
+		require => [
+			Package["ngircd"],
+			File['/etc/openvpn/cacert/dh2048.pem'],
+			File['/etc/ngircd/irc.erebot.net.pem'],
+			File['/etc/ngircd/irc.erebot.net.key'],
+		],
+		notify => Service['ngircd'],
+	}
+
+	file {
+	"/etc/ngircd/irc.erebot.net.pem":
+		source => 'puppet:///modules/ngircd/ssl/irc.erebot.net.pem',
+		notify => Service['ngircd'],
+	}
+
+	file {
+	"/etc/ngircd/irc.erebot.net.key":
+		source => 'puppet:///modules/ngircd/ssl/irc.erebot.net.key',
+		notify => Service['ngircd'],
+		mode => 0600,
+		owner => root,
+		group => root,
+	}
+}
